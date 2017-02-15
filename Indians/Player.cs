111b4a1da24573e029446ea4ac76f498c9e1bcd4
Jed Clarke <c3163676@uon.edu.au>
2017-02-15 20:15:47 +1100

    Add project files.

---
+++ Indians/Player.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indians
+{
+
+    //Each Player object will hold current dynamic information for each player in the current game/round(most likely just that round)
+    //this information will either be commited to the scoresheet as a running total or stored in the database (eventually)
+    class Player
+    {
+        string name;    //players display name
+        int call;       //current call
+        int tricks;     //tricks currently won
+        bool dealer;    //is the player the current dealer? potentially a variable for the scoresheet rather than the player
+        int forced;      //counts previous number of zeros, if = 3 then can't go zero
+        int total;      //the players current total score
+
+        //basic (defualt) constructor, is given a player name and whether or not that player is the dealer (maybe not dealer)
+        public Player(string n, bool d)
+        {
+            name = n;
+            dealer = d;
+            forced = 0;
+        }
+
+        //Destructor
+        ~Player()
+        {
+            //cleanse purge kill
+        }
+        
+        //checks if the current call is valid and assigns it to call, if invalid returns false to get another call
+        public bool makeCall(int c)
+        {
+            if (forced == 3 && c == 0)  //can't call 0 if three zeros already called
+            {
+                return false;   //false returned, invalid call
+            }
+            else
+            {
+                call = c;   //call is assigned
+                return true;    //call is valid, return true
+            }
+        }
+
+        //the player has won a trick so the trick value will be incremented
+        //possibly change to bool to confirm valid entry, if that situation arrises where a trick will be invalid
+        public void trickWon()
+        {
+            tricks++;   //self explanitory
+        }
+
+        //calculates the players new score, this should be used at the end of the round, this will do all the end of the round cleanup, delete tricks and calls
+        //eventually tricks and calls and so forth will be stored in the mythical database that will definitely exist one day
+        public void recalculateScore()
+        {
+            if (tricks == call)
+            {
+                total += 10 * (call + 1);
+            }
+            else
+            {
+                //this seems like an awful way of doing things...
+                //in future I'll write my own darn divide and round function
+                total = (int)Math.Ceiling(((decimal)total)/2);
+            }
+        }
+
+        //assigns player as current dealer, may need to be done elsewhere as dealer is unique, unsure
+        public void isDealer(bool d)
+        {
+            dealer = d;
+        }
+
+    }
+}
---
+++ Indians/ScoreSheet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indians
+{
+    //the scoresheet class will keep track of the current round and the total scores, I think all previous rounds in the current game should be stored in the database, maybe
+    //don't really want to store every previous round within the object, rather unnecessary I think
+    //actually each player should have their current total, stupid to store it seperately
+    class ScoreSheet
+    {
+        List<Player> players;    //list of players
+        int totalPlayers;        //total current players
+
+        //constructor with total number of players, might implement later, not sure
+        public ScoreSheet(int pl)
+        {
+            totalPlayers = pl;
+            players = new List<Player>();
+        }
+
+        //constructor that takes the dealer as the only input, all other players added later
+        public ScoreSheet(string d)
+        {
+            totalPlayers = 1;
+            players = new List<Player>();
+            players.Add(new Player(d, true));
+        }
+
+        //adds a player, returns true if successfully added
+        // will return false if player name is taken or some other fail conditions
+        public bool addPlayer(string n)
+        {
+            //if player name is not already taken, check that later
+            players.Add(new Player(n, false));
+            return true;
+        }
+    }
+}
