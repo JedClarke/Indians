8b86810ae98bbaacfd8a07824ab8108adae272b4
Jed Clarke <c3163676@uon.edu.au>
2017-02-21 17:43:20 +1100

    gets player names

--- Indians/Indians.cs
+++ Indians/Indians.cs
@@ -12,9 +12,19 @@ namespace Indians
         {
             int players;
 
+            //starts game by creating a scoresheet for the current round and then getting player names
+
             Console.WriteLine("Please enter the number of players: ");
             players = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Please enter name for player 1: ");
+            ScoreSheet currentRound = new ScoreSheet(Console.ReadLine());
+
+            for (int p = 2; p <= players; p++ )
+            {
+                Console.WriteLine("Please enter name for player {0}: ", p);
+                currentRound.addPlayer(Console.ReadLine()); //will eventually check for duplicate names, not right now
+            }
         }
     }
 }
